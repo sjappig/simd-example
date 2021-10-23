3abc08337a478239ae6c401032c5378961287b46
Jari-Pekka Ryynänen <jappi85@gmail.com>
2021-10-23 15:56:39 +0300

    Add AVX2 version

--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,52 @@
 
 int16_t y[data::yLen + 3]{};
 
-void naive() {
-    for (auto t = 0; t < data::yLen; ++t) {
-        y[t] = 0;
-        for (auto i = 0; i < data::filterLen; ++i) {
-            y[t] += data::filter[i] * data::x[4 + t - i];
+// filter[0] * x[4 + t]
+// filter[1] * x[3 + t]
+// filter[2] * x[2 + t]
+// filter[3] * x[1 + t]
+void naive(int n) {
+    for (int round = 0; round < n; ++round) {
+        for (auto t = 0; t < data::yLen; ++t) {
+            y[t] = 0;
+            for (auto i = 0; i < data::filterLen; ++i) {
+                y[t] += data::filter[i] * data::x[4 + t - i];
+            }
         }
     }
 }
 
-void sse2() {
-    // filter[0] * x[4 + t]
-    // filter[1] * x[3 + t]
-    // filter[2] * x[2 + t]
-    // filter[3] * x[1 + t]
-    // filter[4] * x[t]
+void sse2(int n) {
     // reverse filter (not really needed since it symmetrical)
-    __m128i tmpFilter = _mm_set_epi16(data::filter[4], data::filter[3], data::filter[2], data::filter[1], data::filter[0], 0, 0, 0);
-    for (auto t = 0; t < data::yLen; ++t) {
-        auto x = _mm_set_epi16(data::x[t], data::x[t + 1], data::x[t + 2], data::x[t + 3], data::x[t + 4], 0, 0, 0);
-        auto tmp = _mm_madd_epi16(tmpFilter, x);
-        tmp = _mm_hadd_epi32(tmp, tmp);
-        tmp = _mm_hadd_epi32(tmp, tmp);
-        y[t] = _mm_extract_epi32(tmp, 0);
+    const __m128i filter = _mm_set_epi16(data::filter[4], data::filter[3], data::filter[2], data::filter[1], data::filter[0], 0, 0, 0);
+    for (int round = 0; round < n; ++round) {
+        for (auto t = 0; t < data::yLen; ++t) {
+            __m128i x = _mm_set_epi16(data::x[t], data::x[t + 1], data::x[t + 2], data::x[t + 3], data::x[t + 4], 0, 0, 0);
+            __m128i tmp = _mm_madd_epi16(filter, x);
+            tmp = _mm_hadd_epi32(tmp, tmp);
+            tmp = _mm_hadd_epi32(tmp, tmp);
+            y[t] = _mm_extract_epi32(tmp, 0);
+        }
+    }
+}
+
+void avx2(int n) {
+    const __m256i filter = _mm256_set_epi16(
+            data::filter[4], data::filter[3], data::filter[2], data::filter[1], data::filter[0], 0, 0, 0,
+            data::filter[4], data::filter[3], data::filter[2], data::filter[1], data::filter[0], 0, 0, 0
+    );
+    for (int round = 0; round < n; ++round) {
+        for (auto t = 0; t < data::yLen; t += 2) {
+            __m256i x = _mm256_set_epi16(
+                    data::x[t + 1], data::x[t + 2], data::x[t + 3], data::x[t + 4], data::x[t + 5], 0, 0, 0,
+                    data::x[t + 0], data::x[t + 1], data::x[t + 2], data::x[t + 3], data::x[t + 4], 0, 0, 0
+            );
+            __m256i tmp = _mm256_madd_epi16(filter, x);
+            tmp = _mm256_hadd_epi32(tmp, tmp);
+            tmp = _mm256_hadd_epi32(tmp, tmp);
+            y[t + 0] = _mm256_extract_epi32(tmp, 0);
+            y[t + 1] = _mm256_extract_epi32(tmp, 4);
+        }
     }
 }
 
@@ -40,7 +63,7 @@ void validateY() {
 }
 
 int main(int argc, char** argv) {
-    void (*targetFunction)() = naive;
+    void (*targetFunction)(int) = naive;
 
     if (argc > 1)
     {
@@ -49,19 +72,22 @@ int main(int argc, char** argv) {
         if (firstArg == "--naive") {
             targetFunction = naive;
         }
-        if (firstArg == "--sse2") {
+        else if (firstArg == "--sse2") {
             targetFunction = sse2;
         }
+        else if (firstArg == "--avx2") {
+            targetFunction = avx2;
+        }
         else {
-            std::cerr << "usage: " << argv[0] << " [--naive|--sse2]" << std::endl;
+            std::cerr << "usage: " << argv[0] << " [--naive|--sse2|--avx2]" << std::endl;
             std::cerr << "unknown argument: " << firstArg << std::endl;
             return -1;
         }
     }
-    for (uint64_t i = 0; i < 1000000; ++i) {
-        targetFunction();
-    }
+    targetFunction(1);
     validateY();
+
+    targetFunction(10000000);
     return 0;
 }
 
